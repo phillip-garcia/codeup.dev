7a93116966963dd35865a35514df673ad2490600
Phillip Garcia <phillipgarcia@Phillips-MacBook-Pro-2.local>
2016-06-06 14:15:02 -0500

    adding object exercise

---
+++ public/js/objects.js
@@ -0,0 +1,20 @@
+(function(){
+    "use strict";
+
+    // TODO: Create person object
+    // var person = todo;
+
+
+    // TODO: Create firstName and lastName properties in your person object; assign your name to them
+
+
+    var person = {};
+	person.firstName = "Phillip";
+	person.lastName = "Garcia";
+	alert("Hello! " + person.firstName + " " + person.lastName);
+    // TODO: Add a sayHello method to the person object that
+    //       alerts a greeting using the firstName and lastName properties
+
+    // Say hello from the person object.
+    // person.sayHello();
+})();
